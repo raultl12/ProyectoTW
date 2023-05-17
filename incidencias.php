a98657160a9deef367a37049246449f29f961c35
Raul Torrente <raultl6212@gmail.com>
2023-05-17 10:01:27 +0200

    Archivo inciencias.php
    Falta añadir la funcion de generacion de codigo de la incidencia. Para mas adelante, primero dejar bien en html

---
+++ incidencias.php
@@ -0,0 +1,9 @@
+<?php
+    require_once 'funciones.php';
+
+    HTMLInicio();
+    MostrarHeader("anonimo"); //Cambiar por el valor de la cookie de sesion
+    //Poner aqui el codigo de generacion de incidencia
+    MostrarFooter();
+    HTMLFin();
+?>
