cee809de86d77d5555858e5c45fce93c033e57b5
Raul Torrente <raultl6212@gmail.com>
2023-05-30 19:22:08 +0200

    intento de conexion a BD

--- php/funcionesBD.php
+++ php/funcionesBD.php
@@ -2,4 +2,21 @@
     /************************************************************************************************************** */
     // Funciones de acceso a Base de Datos
 
+    phpinfo();
+
+    function ObtenerDatosUsuario(){
+        $db = mysqli_connect("https://void.ugr.es/mywebsql/","raultlopez2223","AdiGd24D","raultlopez2223");
+        if ($db) {
+            echo "<p>Conexión con éxito</p>";
+        } else {
+            echo "<p>Error de conexión</p>";
+            echo "<p>Código: ".mysqli_connect_errno()."</p>";
+            echo "<p>Mensaje: ".mysqli_connect_error()."</p>";
+            die("Adiós");
+        }
+        // Establecer el conjunto de caracteres del cliente
+        mysqli_set_charset($db,"utf8");
+    }
+
+    ObtenerDatosUsuario();
 ?>
